aaa9245f3de06d15b1988879f59674e9a536aad1
Mac <kingmuffyatse@gmail.com>
2023-12-27 22:23:40 +0000

    ree services

---
+++ components/Navbar.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+import { useState } from "react";
+import { X } from "lucide-react";
+
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const handlenav = () => {
+    setShowNav(!showNav);
+  };
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/1.png" alt="logo" width={74} height={29} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <button onClick={handlenav}>
+        <Image
+          src="menu.svg"
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
+        />
+      </button>
+      <div
+        className={
+          showNav
+            ? "fixed  left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ffffff] p-10 ease-in duration-500"
+            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+        }
+      >
+        <div className="flex w-full items-center justify-end ">
+          <div onClick={handlenav} className="cursor-pointer">
+            <X />
+          </div>
+        </div>
+        <ul className="flex flex-col gap-10">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
